59c67c0936737dce278acfe53c8ba81a9057f222
Nati Guida <natalia.guida@mercadolibre.com>
2020-02-04 14:23:23 -0300

    Updates

---
+++ precache-manifest.14c6eda53694b39578a52e48c0d6e851.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c00a9059f57151ad1c37958248cca4",
+    "url": "/Arrow-Game/index.html"
+  },
+  {
+    "revision": "8c3d03674299fe811fca",
+    "url": "/Arrow-Game/static/css/main.2ec7d1bf.chunk.css"
+  },
+  {
+    "revision": "2d98dde6cabf5e2aaad9",
+    "url": "/Arrow-Game/static/js/2.472cce64.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Arrow-Game/static/js/2.472cce64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c3d03674299fe811fca",
+    "url": "/Arrow-Game/static/js/main.084badfd.chunk.js"
+  },
+  {
+    "revision": "9c32ce9b8f35219f3a02",
+    "url": "/Arrow-Game/static/js/runtime-main.b554b8ca.js"
+  },
+  {
+    "revision": "3f7dcb264632bc7fa175fd1c28a93c6b",
+    "url": "/Arrow-Game/static/media/brillante.3f7dcb26.png"
+  },
+  {
+    "revision": "9ebfda75b426493500729759fb90a007",
+    "url": "/Arrow-Game/static/media/twitter.9ebfda75.png"
+  }
+]);
