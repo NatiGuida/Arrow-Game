3107b82a4c6ad536d88d8d8f01d32c54cc9f4a5e
Nati Guida <natalia.guida@mercadolibre.com>
2020-02-04 14:21:11 -0300

    package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './wrappers/application';
+import Application from './wrappers/application';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('root'));
 
